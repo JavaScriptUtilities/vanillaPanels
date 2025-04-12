153acf9676da2e5894e131ab7b02c25258accd3f
Kevin <kevin@rocher-dev.com>
2025-04-12 22:40:10 +0200

    v 0.2.0
    - Added functionality to build panels dynamically from HTML elements with data attributes.

--- js/vanilla-panels.js
+++ js/vanilla-panels.js
@@ -1,6 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
     'use strict';
 
+    var _panelTemplate =
+        '<div class="panel-wrapper" aria-hidden="true" data-panel-open="false">' +
+        '<div class="panel-overlay"></div>' +
+        '<div class="panel-content">' +
+        '<a href="#" class="panel-content__close"><span>Close Panel</span></a>' +
+        '</div>' +
+        '</div>';
+
+    var $panelsToBuild = document.querySelectorAll('[data-build-panel]');
+    Array.prototype.forEach.call($panelsToBuild, function($item) {
+        if (!$item) {
+            return;
+        }
+
+        if (!$item.querySelector('.panel-content__inner')) {
+            console.log('No panel content found');
+            return;
+        }
+
+        var _panelId = $item.getAttribute('data-build-panel');
+
+        /* Build wrapper */
+        var $wrapper = document.createElement('div');
+        $wrapper.innerHTML = _panelTemplate;
+        $wrapper = $wrapper.firstElementChild;
+        $wrapper.setAttribute('data-panel', _panelId);
+
+        /* Load panel content */
+        $item.removeAttribute('data-build-panel');
+        $wrapper.querySelector('.panel-content').appendChild($item);
+
+        /* Inject in body */
+        document.body.appendChild($wrapper);
+    });
+
     /* Open a panel on click */
     document.body.addEventListener('click', function(event) {
         var panelTrigger = event.target.closest('[data-panel-trigger]');
